edd9a75b09ed8953bc284e6366bf06e9a429270b
shantanupatil23 <kanhapatildurg@gmail.com>
2021-01-28 11:59:30 +0530

    create flutter project

---
+++ APP/android/app/src/main/kotlin/com/example/click_to_cook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.click_to_cook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ APP/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+Color primaryAppColor = Color(0xffffbe00);
+Color secondaryAppColor = Color(0xff1c819e);
+Color accentColor = Color(0xff005542);
+String appName = 'Click 2 Cook';
