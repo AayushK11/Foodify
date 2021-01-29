cddc51fb35f7136981e6eb6855bfac762a8df9cc
kaizen-cmd <tejasmandre0306@gmail.com>
2021-01-29 18:42:11 +0530

    share-img url added

--- clickToCook_API/endpoints/urls.py
+++ clickToCook_API/endpoints/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("bulk-results/", views.bulk_images),
     path("scrape/", views.get_recipes),
     path("sign-up/", views.signup),
+    path("share-image/", views.recog_share_image),
     path("post-food/", views.post_food),
     path("get-food/", views.get_food),
     path("all-user-post/", views.particular_user_food),
